18440b61ef27586e2c66031d0461c3717001b461
Vladimir Voitenko <vladimirdev635@gmail.com>
2025-02-06 19:49:03 +0500

    fix

--- main.go
+++ main.go
@@ -1,62 +1,46 @@
 package main
 
 import (
-	"github.com/Voldemat/go-smtp-mock/emails"
 	"log"
-	"net/http"
 	"os"
 	"sync"
+
+	"github.com/Voldemat/go-smtp-mock/server"
 )
 
-
 func main() {
-    queueSize := os.Getenv("SMTP_QUEUE_SIZE")
-    if queueSize == "" {
-        queueSize = "10"
-    }
-    smtpHost := os.Getenv("SMTP_HOST")
-    if smtpHost == "" {
-        log.Fatal("SMTP_HOST is not defined")
-    }
-    smtpPort := os.Getenv("SMTP_PORT")
-    if smtpPort == "" {
-        log.Fatal("SMTP_PORT is not defined")
-    }
-    smtpUser := os.Getenv("SMTP_USER")
-    if smtpUser == "" {
-        log.Fatal("SMTP_USER is not defined")
-    }
-    smtpPassword := os.Getenv("SMTP_PASSWORD")
-    if smtpPassword == "" {
-        log.Fatal("SMTP_PASSWORD is not defined")
-    }
-    backend, server := emails.CreateSMTPServer(
+	queueSize := os.Getenv("SMTP_QUEUE_SIZE")
+	if queueSize == "" {
+		queueSize = "10"
+	}
+	smtpHost := os.Getenv("SMTP_HOST")
+	if smtpHost == "" {
+		log.Fatal("SMTP_HOST is not defined")
+	}
+	smtpPort := os.Getenv("SMTP_PORT")
+	if smtpPort == "" {
+		log.Fatal("SMTP_PORT is not defined")
+	}
+	smtpUser := os.Getenv("SMTP_USER")
+	if smtpUser == "" {
+		log.Fatal("SMTP_USER is not defined")
+	}
+	smtpPassword := os.Getenv("SMTP_PASSWORD")
+	if smtpPassword == "" {
+		log.Fatal("SMTP_PASSWORD is not defined")
+	}
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	server.CreateServerRoutines(
+        &wg,
+        queueSize,
         smtpHost,
         smtpPort,
         smtpUser,
         smtpPassword,
-        queueSize,
+        os.Getenv("HTTP_HOST"),
+        os.Getenv("HTTP_PORT"),
     )
-    mux := CreateHTTPServer(backend)
-    wg := sync.WaitGroup{}
-
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-        http.ListenAndServe(
-            os.Getenv("HTTP_HOST") + ":" + os.Getenv("HTTP_PORT"),
-            mux,
-        )
-	}()
-
-	go func() {
-		defer wg.Done()
-        err := server.ListenAndServe()
-        if err != nil {
-            log.Fatal(err)
-        }
-	}()
-
-	wg.Wait()
+    wg.Wait()
 }
---
+++ server/server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/Voldemat/go-smtp-mock/emails"
+)
+
+func CreateServerRoutines(
+    wg *sync.WaitGroup,
+    smtpHost string,
+    smtpUser string,
+    smtpPort string,
+    smtpPassword string,
+    queueSize string,
+    httpHost string,
+    httpPort string,
+) {
+    backend, server := emails.CreateSMTPServer(
+        smtpHost,
+        smtpPort,
+        smtpUser,
+        smtpPassword,
+        queueSize,
+    )
+    mux := CreateHTTPServer(backend)
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+        http.ListenAndServe(
+            httpHost + ":" + httpPort,
+            mux,
+        )
+	}()
+
+	go func() {
+		defer wg.Done()
+        err := server.ListenAndServe()
+        if err != nil {
+            log.Fatal(err)
+        }
+	}()
+}
